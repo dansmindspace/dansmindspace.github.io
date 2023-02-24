dc261acf450c76e2cf4c5faf4ce40fa25edf32a0
Dan Dahl <dan.thelostsorcerer@gmail.com>
2023-02-23 19:06:45 -0700

    Added P5js hosted content

---
+++ assets/hosted/scripts/stamp.js
@@ -0,0 +1,18 @@
+function loadStamp() {
+  return loadImage(stamp)
+}
+
+function recolorStamp(image, oldColor, newColor) {
+  image.loadPixels();
+  for (let i = 0; i < image.pixels.length; i += 4) {
+    if (
+      image.pixels[i] == red(oldColor) &&
+      image.pixels[i + 1] == blue(oldColor) &&
+      image.pixels[i + 2] == green(oldColor)
+    ) {
+      image.pixels[i] = red(newColor);
+      image.pixels[i + 1] = blue(newColor);
+      image.pixels[i + 2] = green(newColor);
+    }
+  }
+  image.updatePixels();
